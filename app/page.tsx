45ee169d900dab19f0afe91f57c55a2ecea0aa32
Hiep Nguyen <hiepnguyentpc@gmail.com>
2025-04-20 17:52:09 -0500

    Small fixes

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default async function Home() {
         <h1 className="font-semibold text-4xl mb-4 text-slate-950">
           Hey there, I’m Henry Nguyen!
           <span className="block text-slate-500 font-normal text-2xl">
-            Software Engineer - Product
+            Software Engineer II - Product
           </span>
         </h1>
         <p className="text-slate-700 text-lg md:text-xl leading-normal">
